ce7f6ac99eb0de7fc5567b494f7778dcfe143454
IkkanWatanabe <110508666+ichi-7@users.noreply.github.com>
2022-09-12 09:11:44 +0000

    [save] kadai-3

--- Bookers2/config/routes.rb
+++ Bookers2/config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
   
-  resources :books, only: [:new, :index, :show, :create, :edit]
-  resources :users, only: [:index, :show, :edit]
+  resources :books, only: [:new, :index, :show, :create, :edit, :destroy, :update]
+  resources :users, only: [:index, :show, :edit, :update]
   
   get 'homes/about' => 'homes#about', as: 'about'
   
